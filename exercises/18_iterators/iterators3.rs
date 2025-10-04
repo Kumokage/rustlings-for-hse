47a1f29b1eccae9a377daf4388c65e30a022b2eb
bivstrog <bivstroganov@gmail.com>
2025-10-04 23:45:05 +0300

    first step

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -11,7 +11,7 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    Ok(a / b)
 }
 
 // TODO: Add the correct return type and complete the function body.
@@ -19,6 +19,7 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
 fn result_with_list() {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    division_results
 }
 
 // TODO: Add the correct return type and complete the function body.
@@ -26,6 +27,7 @@ fn result_with_list() {
 fn list_of_results() {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    division_results
 }
 
 fn main() {
